3dd611f476e63b245a124b7d5e79d2b76b4bc276
poojasshetty92 <poojasid92@gmail.com>
2024-02-08 22:07:21 +0530

    ok

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -1,35 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { StateService } from '../state.service';
-
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.page.html',
   styleUrls: ['./add.page.scss'],
 })
-export class AddPage implements OnInit {
+export class AddPage {
 
-  id: any='';
-  name: any='';
+  name: string = '';
 
   constructor(private router: Router, private stateService: StateService) { }
 
-  ngOnInit() {
-  }
   addState() {
-    let state = {
-      id: this.id,
-      Name: this.name
-      
-    };
-    if(this.id!='' && this.name!='') {
-      this.stateService.addState(state).then(data => {
-        console.log(data);
-      });
+    // Trim the state name and check if it's not empty
+    const trimmedName = this.name.trim();
+    if (trimmedName) {
+      // Create the state object with the trimmed name
+      const state = { name: trimmedName };
+
+      // Call the addState method of the StateService
+      this.stateService.addState(state)
+        .then((data: any) => {
+          console.log('State added successfully:', data);
+          this.router.navigate(['home']); // Navigate to home page after successful addition
+        })
+        .catch((error: any) => {
+          console.error('Error adding state:', error);
+          // Handle error (e.g., show error message to user)
+        });
+    } else {
+      // Handle case where name is blank (e.g., show error message to user)
     }
-    this.router.navigate(['home']);
   }
-
-
 }
--- src/app/state.service.ts
+++ src/app/state.service.ts
@@ -1,57 +1,53 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ErrorHandler } from '@angular/core';
-import { identity } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class StateService {
+  apiBaseUrl: string = 'http://15.207.84.40:3000/api/';
 
-  apiBaseUrl:string='';
+  constructor(public http: HttpClient) {}
 
-  constructor(public http: HttpClient) {
-    this.apiBaseUrl = 'http://15.207.84.40:3000/';
-  }
-
-  getState(USN: any) {
-    return new Promise(resolve => {
-      this.http.get(this.apiBaseUrl + 'states/' + identity).subscribe((data) => {
-        resolve(data);
-      })
+getState(USN: any) {
+  return new Promise(resolve => {
+    this.http.get(this.apiBaseUrl + 'states/' + USN).subscribe((data: any) => {
+      console.log('Server Response:', data); // Log the response data
+      resolve(data.name); // Assuming the server response contains a 'name' property
     });
-  }
+  });
+}
+
 
   getStates() {
     return new Promise(resolve => {
-      this.http.get(this.apiBaseUrl+'states/').subscribe(data => {
+      this.http.get(this.apiBaseUrl + 'states/').subscribe(data => {
         resolve(data);
-      })
+      });
     });
   }
 
   addState(state: any) {
     return new Promise(resolve => {
-      this.http.post(this.apiBaseUrl+'states/', state).subscribe(data => {
+      this.http.post(this.apiBaseUrl + 'states/', state).subscribe(data => {
         resolve(data);
-      })
+      });
     });
   }
 
   updateState(USN: any, state: any) {
     return new Promise(resolve => {
-      this.http.put(this.apiBaseUrl+'states/'+identity, state).subscribe(data => {
+      this.http.put(this.apiBaseUrl + 'states/' + USN, state).subscribe(data => {
         resolve(data);
-      })
+      });
     });
   }
 
   deleteState(USN: any) {
     return new Promise(resolve => {
-      this.http.delete(this.apiBaseUrl+'states/'+identity).subscribe(data => {
+      this.http.delete(this.apiBaseUrl + 'states/' + USN).subscribe(data => {
         resolve(data);
-      })
+      });
     });
   }
 }
